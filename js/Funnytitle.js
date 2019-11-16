441bb8af06d03210a9829a565cd3e131749bf913
zhixingheyi1102 <1004230628@qq.com>
2019-11-16 17:17:19 +0800

    Site updated: 2019-11-16 17:17:17

--- js/Funnytitle.js
+++ js/Funnytitle.js
@@ -3,12 +3,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'vivi的奇幻世界！';
+         document.title = '你不爱我了吗qaq';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '你不爱我了吗' + OriginTitle;
+         document.title = 'vivi的奇幻世界' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
