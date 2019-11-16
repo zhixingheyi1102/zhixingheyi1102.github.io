2b98b27485f62252e7b9c9f4eda06fec9f9dfd74
zhixingheyi1102 <1004230628@qq.com>
2019-11-16 18:16:58 +0800

    Site updated: 2019-11-16 18:16:57

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,14 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
+        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+        clearTimeout(titleTime);
+     }ヽ(●-`Д´-)ノ你丑你就走
  });
